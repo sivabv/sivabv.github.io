afb8c8b2f6139a653b3f7ac90fb4faae1e197bc5
siva kumar <sivabv13@gmail.com>
2016-06-20 18:44:15 -0400

    Ajax Demo- list

---
+++ javascripts/functions.js
@@ -0,0 +1,16 @@
+console.log('This would be the main JS file.');
+$(document).ready(function(){
+
+var url = 'http://jsfiddle.net/api/user/tycahill/demo/list.json?callback=?';
+
+// Get data from jsFiddle
+$.getJSON( url, function(data) {
+
+    // Loop throught the "list" array in the data
+    $.each(data.list, function(i, listElement){
+        // Put the title of each list element on the page
+        $('body').append(listElement.title + '<br>');
+    })
+});
+           
+})
